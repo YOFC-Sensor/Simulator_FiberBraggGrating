2c9c16597c57971b59e6638e74a437a499046b8e
星堡 付 <fxb19960829@163.com>
2021-05-10 12:11:07 +0800

    FXB Commit

---
+++ ModbusTCP_Server/ModbusTCP_Server/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModbusTCP_Server
+{
+    public partial class Server_Form : Form
+    {
+        public Server_Form()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
